10bcdb1c9f127e5ddfbd29d71b5198781c7e7573
guilherme barbosa lima <guilhermebali@gmail.com>
2022-09-23 14:33:57 -0300

    add interaction repeat

--- src/Pegar.java
+++ src/Pegar.java
@@ -15,52 +15,57 @@ public class Pegar {
 		Multiplicar mt = new Multiplicar();
 		Divisao dv = new Divisao();
 		
-		int valueBeginPrime, valueBeginSecond = 0;
+		int valueBeginPrime = 0;
+		int valueBeginSecond = 0;
 		
-		int operation = 0;
+		int operation = 0;		
 		
-		operation = sc.nextInt();
-		
-		valueBeginPrime = sc.nextInt();
-		valueBeginSecond = sc.nextInt();					
-		
-		if (operation == 1) {
-			sm.setValuePrime(valueBeginPrime);
-			sm.setValueSecond(valueBeginSecond);
-			
-			System.out.println(sm.sum());
-			
-			operation = 0;
-		}
-		
-		if( operation == 2) {
-			sb.setValuePrime(valueBeginPrime);
-			sb.setValueSecond(valueBeginSecond);
-			
-			System.out.println(sb.subraction());
-			
-			operation = 0;
-		}
-		
-		if( operation == 3) {
-			mt.setValuePrime(valueBeginPrime);
-			mt.setValueSecond(valueBeginSecond);
-			
-			System.out.println(mt.multiplication());
-			
-			operation = 0;
-		}
-		
-		if( operation == 4) {
-			dv.setValuePrime(valueBeginPrime);
-			dv.setValueSecond(valueBeginSecond);
-			
-			System.out.println(dv.division());
-			
-			operation = 0;
-		}
+		do {
+			System.out.println("Which operation will you want??");
+			System.out.println("1 - Sum");
+			System.out.println("2 - Subtraction");
+			System.out.println("3 - Multiplication");
+			System.out.println("4 - Division");
+			System.out.println("5 - Exit");
 
-		if (operation == 0) {
+			operation = sc.nextInt();	
+			
+			if (operation < 5) {
+				valueBeginPrime = sc.nextInt();
+				valueBeginSecond = sc.nextInt();				
+			}		
+			
+			if (operation == 1) {
+				sm.setValuePrime(valueBeginPrime);
+				sm.setValueSecond(valueBeginSecond);
+				
+				System.out.println(sm.sum());
+			}
+			
+			if( operation == 2) {
+				sb.setValuePrime(valueBeginPrime);
+				sb.setValueSecond(valueBeginSecond);
+				
+				System.out.println(sb.subraction());
+			}
+			
+			if( operation == 3) {
+				mt.setValuePrime(valueBeginPrime);
+				mt.setValueSecond(valueBeginSecond);
+				
+				System.out.println(mt.multiplication());
+			}
+			
+			if( operation == 4) {
+				dv.setValuePrime(valueBeginPrime);
+				dv.setValueSecond(valueBeginSecond);
+				
+				System.out.println(dv.division());
+			}
+			
+		}while(operation < 4);
+
+		if (operation > 4) {
 			System.out.print("Fim");
 		}		
 		
